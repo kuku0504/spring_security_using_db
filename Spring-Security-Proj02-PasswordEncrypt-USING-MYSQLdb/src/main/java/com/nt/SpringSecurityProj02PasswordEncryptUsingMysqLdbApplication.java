2dd5dbd506f943c0844e30a8a863e86d00b2db61
dev2 <developer2@gmail.com>
2025-08-07 22:19:04 +0530

    initial changes

---
+++ Spring-Security-Proj02-PasswordEncrypt-USING-MYSQLdb/src/main/java/com/nt/SpringSecurityProj02PasswordEncryptUsingMysqLdbApplication.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityProj02PasswordEncryptUsingMysqLdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityProj02PasswordEncryptUsingMysqLdbApplication.class, args);
+	}
+
+}
